22153eebde8677a10e22a15de35f9f3d781f85fc
benjaminaaron <b.degenhart.work@gmail.com>
2023-11-03 21:11:39 +0100

    Try moving the CSS folder upwards

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/static/css");
+    eleventyConfig.addPassthroughCopy("src/css");
 
     return {
         dir: {
