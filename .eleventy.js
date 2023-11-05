1da44e49ab957d0b2758dad44cdb5ebc9d4758e5
wbglaeser <ben.glaeser@soundcloud.com>
2023-11-05 18:12:02 +0100

    fix path resolution via elevently js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,19 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css");
 
+    // Default path for local development
+    let rootPath = "/";
+
+    // If environment is production, change the path
+    if(process.env.ELEVENTY_ENV === "production") {
+        rootPath = "/ckg-site/";
+    }
+
+    // Pass the stylesheet path to your templates
+    eleventyConfig.addShortcode("rootPath", function() {
+        return rootPath;
+    });
+
     return {
         dir: {
             input: "src",      // This sets the input directory to "src"
